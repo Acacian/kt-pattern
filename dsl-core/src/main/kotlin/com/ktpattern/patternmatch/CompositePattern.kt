68d2561bfdfa36c26793ae7e8c83cf62842de810
kudoha <kudoha@gmail.com>
2025-04-08 11:45:20 +0900

    Feat : 다중패턴 추가가

---
+++ dsl-core/src/main/kotlin/com/ktpattern/patternmatch/CompositePattern.kt
@@ -0,0 +1,11 @@
+package com.ktpattern.patternmatch
+
+class AndPattern<T>(private val patterns: List<Pattern<T>>) : Pattern<T> {
+    override fun match(value: T): Boolean = patterns.all { it.match(value) }
+    override fun getType(): Class<*> = patterns.firstOrNull()?.getType() ?: Any::class.java
+}
+
+class OrPattern<T>(private val patterns: List<Pattern<T>>) : Pattern<T> {
+    override fun match(value: T): Boolean = patterns.any { it.match(value) }
+    override fun getType(): Class<*> = patterns.firstOrNull()?.getType() ?: Any::class.java
+}
